6b12f96355b0972f79735e186a0b86a3a14653a4
noname068218 <128069790+noname068218@users.noreply.github.com>
2023-11-25 03:49:56 +0100

    end task

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
+import { useSpring, animated } from 'react-spring';
 import { FeedbackWraper, BtnWraper } from './Feedback.style';
 import { Section } from './Title';
 import { FeedbackOptions } from './Button';
@@ -6,57 +7,72 @@ import { Statistics } from './Statistic';
 
 const Notification = ({ message }) => <p>{message}</p>;
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-  countTotalFeedback() {
-    const { good, neutral, bad } = this.state;
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const props = useSpring({
+    opacity: 1,
+    y: 0,
+    from: { opacity: 0, y: -100 },
+  });
+
+  const countTotalFeedback = () => {
     return good + neutral + bad;
-  }
-
-  countPositiveFeedbackPercentage() {
-    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
-  }
-
-  onLeaveFeedback = option => {
-    this.setState(prevState => {
-      return {
-        [option]: prevState[option] + 1,
-      };
-    });
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    return (
-      <div>
-        <FeedbackWraper>
-          <Section title="Please leave feedback">
-            <BtnWraper>
-              <FeedbackOptions
-                onLeaveFeedback={this.onLeaveFeedback}
-                options={this.state}
-              />
-            </BtnWraper>
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
+    return total > 0 ? Math.round((good / total) * 100) : 0;
+  };
+
+  const onLeaveFeedback = option => {
+    switch (option) {
+      case 'good':
+        setGood(prevGood => prevGood + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevNeutral => prevNeutral + 1);
+        break;
+      case 'bad':
+        setBad(prevBad => prevBad + 1);
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <animated.div
+      style={{
+        opacity: props.opacity,
+        transform: props.y.interpolate(y => `translateY(${y}px)`),
+      }}
+    >
+      <FeedbackWraper>
+        <Section title="Please leave feedback">
+          <BtnWraper>
+            <FeedbackOptions
+              options={{ good: 'Good', neutral: 'Neutral', bad: 'Bad' }}
+              onLeaveFeedback={onLeaveFeedback}
+            />
+          </BtnWraper>
+        </Section>
+        {countTotalFeedback() > 0 ? (
+          <Section title="Statistics">
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={countTotalFeedback()}
+              positive={countPositiveFeedbackPercentage()}
+            />
           </Section>
-          {this.countTotalFeedback() > 0 ? (
-            <Section title="Statistics">
-              <Statistics
-                good={good}
-                neutral={neutral}
-                bad={bad}
-                total={this.countTotalFeedback()}
-                positive={this.countPositiveFeedbackPercentage()}
-              />
-            </Section>
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </FeedbackWraper>
-      </div>
-    );
-  }
-}
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </FeedbackWraper>
+    </animated.div>
+  );
+};
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,10 @@
+// FeedbackOptions.js
 import { StyledButton } from './Feedback.style';
 
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return Object.keys(options).map(key => (
     <StyledButton key={key} onClick={() => onLeaveFeedback(key)}>
-      {key}
+      {options[key]}
     </StyledButton>
   ));
 };
